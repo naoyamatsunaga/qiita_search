f2bac80e809361d18657b7e84099796306b0d790
NaoyaMatsunaga <>
2024-01-30 23:37:24 +0900

    検索ボックスの

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:qiita_search/screens/search_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
@@ -10,9 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Qiita Search', //タイトル
       theme: ThemeData(
+        primarySwatch: Colors.green,
+        fontFamily: 'Hiragino Sans',
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF55C500),
         ),
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white,
+            ),
       ),
+      home: const SearchScreen(),
     );
   }
 }

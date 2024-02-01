10182b1fac0b5c6b97b5f9b2ad5c9983a2d4842f
NaoyaMatsunaga <>
2024-02-01 23:58:58 +0900

    画面遷移を実装

--- lib/widgets/article_container.dart
+++ lib/widgets/article_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:qiita_search/models/article.dart';
+import 'package:qiita_search/screens/article_screen.dart';
 
 class ArticleContainer extends StatelessWidget {
   // コンストラクタ
@@ -15,97 +16,105 @@ class ArticleContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(
-        vertical: 12,
         horizontal: 16,
+        vertical: 12,
       ),
-      child: Container(
-        height: 180,
-        padding: const EdgeInsets.symmetric(
-          // 余白の指定
-          horizontal: 20,
-          vertical: 16,
-        ),
-        decoration: const BoxDecoration(
-          // 背景色の指定
-          color: Color(0xFF55C500),
-          borderRadius: BorderRadius.all(
-            // 角丸を設定
-            Radius.circular(32),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: ((context) => ArticleScreen(article: article)),
+            ),
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            // 余白の指定
+            horizontal: 20,
+            vertical: 16,
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // 投稿日
-            Text(
-              DateFormat("yyyy/MM/dd").format(article.createdAt),
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 12,
-              ),
+          decoration: const BoxDecoration(
+            // 背景色の指定
+            color: Color(0xFF55C500),
+            borderRadius: BorderRadius.all(
+              // 角丸を設定
+              Radius.circular(32),
             ),
-            // タイトル
-            Text(
-              article.title,
-              maxLines: 2, // 行数制限
-              overflow: TextOverflow.ellipsis, // 表示範囲超えた時の表示方法の設定
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // 投稿日
+              Text(
+                DateFormat("yyyy/MM/dd").format(article.createdAt),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                ),
               ),
-            ),
-            // タグ
-            Text(
-              '#${article.tags.join(' #')}', // ←文字列の配列をjoinで結合
-              style: const TextStyle(
-                fontSize: 12,
-                color: Colors.white,
-                fontStyle: FontStyle.italic, // フォントスタイルを斜体に設定
+              // タイトル
+              Text(
+                article.title,
+                maxLines: 2, // 行数制限
+                overflow: TextOverflow.ellipsis, // 表示範囲超えた時の表示方法の設定
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                // ハートアイコンといいね数
-                Column(
-                  children: [
-                    const Icon(
-                      Icons.favorite, // ハートアイコン
-                      color: Colors.white,
-                    ),
-                    Text(
-                      article.likesCount.toString(),
-                      style: const TextStyle(
-                        fontSize: 12,
+              // タグ
+              Text(
+                '#${article.tags.join(' #')}', // ←文字列の配列をjoinで結合
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic, // フォントスタイルを斜体に設定
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  // ハートアイコンといいね数
+                  Column(
+                    children: [
+                      const Icon(
+                        Icons.favorite, // ハートアイコン
                         color: Colors.white,
                       ),
-                    ),
-                  ],
-                ),
-                // 投稿者のアイコンと名前
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    CircleAvatar(
-                      radius: 26,
-                      backgroundImage:
-                          NetworkImage(article.user.profileImageUrl),
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      article.user.id,
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.white,
+                      Text(
+                        article.likesCount.toString(),
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                    ],
+                  ),
+                  // 投稿者のアイコンと名前
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CircleAvatar(
+                        radius: 26,
+                        backgroundImage:
+                            NetworkImage(article.user.profileImageUrl),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        article.user.id,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
